60a1b283e248adffb4bd935d2f28d3b52dbe3cc6
Yehor.Bukhanets <Yehor.Bukhanets@infopulse.com>
2022-05-10 22:03:37 +0300

    token salt bug fixed

--- src/main/java/com/fueladvisor/fuelappuserservice/model/dto/RegistrationDto.java
+++ src/main/java/com/fueladvisor/fuelappuserservice/model/dto/RegistrationDto.java
@@ -5,12 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegistrationDto {
+    @Email
     private String email;
+    @Min(6)
     private String password;
+    @Min(6)
     private String confirmPassword;
 }
