2b0624dff1b619311303343d018770cb8c5e5f41
Yehor.Bukhanets <Yehor.Bukhanets@infopulse.com>
2022-05-14 11:48:52 +0300

    fixed bug with request mapping

--- src/main/java/com/fueladvisor/fuelappuserservice/controller/UserController.java
+++ src/main/java/com/fueladvisor/fuelappuserservice/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 
 @RestController
-@RequestMapping("/api/v1//user")
+@RequestMapping("/api/v1/user")
 public class UserController {
     private final UserService userService;
 
